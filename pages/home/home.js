ef87394ffab807df9ddaef3a7a1899c4b43df58b
BeatrizHCor <beatrizhcoradi@gmail.com>
2023-05-17 19:43:04 -0300

    Mais Livros

--- pages/home/home.js
+++ pages/home/home.js
@@ -4,9 +4,18 @@ function openBook(){
   cover = document.getElementById("cover")
   card = document.getElementById("card")
   login = document.getElementById("login")
+  registro = document.getElementById("registro")
+  cover.classList.remove("closed");
+  card.classList.remove("closed");
+  login.classList.remove("closed")
+  registro.classList.remove("closed")
+  for(i = 0; i < pages.length -1; i++){
+    pages[i].classList.remove("closed")
+  }
   cover.classList.add("turn");
   card.classList.add("turn");
   login.classList.add("turn")
+  registro.classList.add("turn")
   for(i = 0; i < pages.length -1; i++){
     pages[i].classList.add("turn")
   }
@@ -16,3 +25,33 @@ function openBook(){
 
 
 document.getElementById("registrar").addEventListener("click", openBook)
+
+
+
+function closeBook(){
+  pages = document.getElementsByClassName("page")
+  cover = document.getElementById("cover")
+  card = document.getElementById("card")
+  login = document.getElementById("login")
+  registro = document.getElementById("registro")
+  cover.classList.remove("turn");
+  card.classList.remove("turn");
+  login.classList.remove("turn")
+  registro.classList.remove("turn")
+  for(i = 0; i < pages.length -1; i++){
+    pages[i].classList.remove("turn")
+  }
+  cover.classList.add("closed");
+  card.classList.add("closed");
+  login.classList.add("closed")
+  registro.classList.add("closed")
+  for(i = 0; i < pages.length -1; i++){
+    pages[i].classList.add("closed")
+  }
+
+}
+
+
+
+
+document.getElementById("Voltar").addEventListener("click", closeBook)
